0dc4a35ca468acfd1861fe5b8f7a3d3d15d5c488
Alan Ramirez <erovicecode590@gmail.com>
2018-03-15 19:56:59 -0700

    changes made

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import pygame
+import sys
 
 from settings import *
 
-class game():
+class Game():
     def __init__(self):
         pygame.init()
+        self.running = True
         screen = pygame.display.set_mode(DISPLAY)
+
+    def run(self):
+        self.events()
+        self.update()
+        self.draw()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pygame.screen.fill(BLACK)
+
+g = Game()
+while g.running:
+    g.run()
+
+pygame.quit(
+sys.exit()
+)
--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 WIDTH = 800
 HEIGHT = 600
+FPS = 60
 
 DISPLAY = (WIDTH, HEIGHT)
+
+# Define Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
