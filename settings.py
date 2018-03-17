c0b05fb7c21f22b31a350dd404798a36e3f57d96
Alan Ramirez <erovicecode590@gmail.com>
2018-03-17 10:44:01 -0700

    Added comments and description for what is being defined.

--- settings.py
+++ settings.py
@@ -1,19 +1,29 @@
+#==================================================
+#description    :Properties for sprites, game and
+#                tile map are defined here.
+#==================================================
+# Import module to run pygame.
 import pygame as pg
 
-# Define window and game properties
+# Window width and height.
 WIDTH = 1032
 HEIGHT = 786
 DISPLAY = (WIDTH, HEIGHT)
-FPS = 60
 
-# Define Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-LIGHTGREY = (150, 150, 150)
-DARKGREY = (50, 50, 50)
+FPS = 60 # Define frames per second.
 
-# Define tile size.
+# Colors are defined with respect to RGB (Red, Green, Blue).
+                   R    G    B
+BLACK           = (0,   0,   0)
+WHITE           = (255, 255, 255)
+LIGHTGREY       = (150, 150, 150)
+DARKGREY        = (50,  50,  50)
+RED             = (255,  0,  0)
+GREEN           = (0,  255,  0)
+BLUE            = (0,  0,  255)
+
+# tile size to adjust pixel size to.
 TILESIZE = 32
 
-# Player settings
+# Player Properties.
 PLAYER_SPEED = 150
