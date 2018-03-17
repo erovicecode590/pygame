7e8b01f98e34427c62f82406f5f02fa7c41af222
Alan Ramirez <erovicecode590@gmail.com>
2018-03-17 09:22:36 -0700

    Added comments to main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+#============================================================
+#title              :tile based game script
+#description        :template for a pygame script
+#author             :Alan Ramirez
+#date               :date
+#usage              :python main.py
+#notes              :
+#python_version     :3.6.2
+#============================================================
+
+# Import pygame module needed to run script
 import pygame as pg
 import sys
 
+# Import path to join files from other directories
 from os import path
+
+# Import everything from other py files in directory
 from settings import *
 from sprites import *
 
+# Create a game object and define methods to call.
 class Game():
     def __init__(self):
         pg.init()
@@ -13,18 +28,21 @@ class Game():
         self.screen = pg.display.set_mode(DISPLAY)
 
     def new_game(self):
+        # Initialize setup of sprites when a new game is started.
         self.all_sprites = pg.sprite.Group()
         self.player = Player(self, 5, 5)
 
     def run(self):
+        # Method which calls other methods while game loop is active.
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS)/1000
+            self.dt = self.clock.tick(FPS)/1000 # time change in the game.
             self.events()
             self.update()
             self.draw()
 
     def events(self):
+        # Keeps track of player/user input from keys/mouse clicks.
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 if self.playing:
@@ -32,11 +50,13 @@ class Game():
                 self.running = False
 
     def update(self):
+        # Updates all sprite movement and changes within groups.
         self.all_sprites.update()
 
     def draw(self):
-        self.screen.fill(DARKGREY)
-        self.all_sprites.draw(self.screen)
+        # Redraw changes on to the screen.
+        self.screen.fill(DARKGREY) # Background color
+        self.all_sprites.draw(self.screen) # Draw all sprites within group on to screen.
         pg.display.flip()
 
     def load_data(self):
@@ -50,6 +70,7 @@ class Game():
 
         self.screen.fill(BLACK)
 
+# Call game object and methods to run script.
 g = Game()
 g.show_start_screen()
 while g.running:
