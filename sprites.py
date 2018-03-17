20c345565fce4a92caba5101050ced4a42f67c2a
Alan Ramirez <erovicecode590@gmail.com>
2018-03-17 10:24:59 -0700

    Added comments to describe methods in player sprite.

--- sprites.py
+++ sprites.py
@@ -1,21 +1,32 @@
+#=============================================================
+# Description   :Sprite classes are created and properties
+#                such as movement and collisions are defined.
+#=============================================================
+
+# Import pygame module to access pygame functions.
 import pygame as pg
 
+# Import properties from settings file.
 from settings import *
 
+# Define 2D vector to use for sprite movement.
 vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
-        self.groups = game.all_sprites
+        # Define initial properties for sprite when created.
+        self.groups = game.all_sprites # Defines group(s) sprite belongs to.
+        # Line necessary for sprite to be initialized in pygame.
         pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
+        self.game = game # Allows sprite to reference self in game object.
         self.image = pg.Surface((30, 40))
         self.image.fill(BLACK)
-        self.rect = self.image.get_rect()
-        self.pos = vec(x, y)*TILESIZE
+        self.rect = self.image.get_rect() # Defines rect to manage sprite placement.
+        self.pos = vec(x, y)*TILESIZE # Sprite placement based on location called to.
         self.vel = vec(0, 0)
 
     def get_keys(self):
+        # Method for obtaining keys pressed by player/user to move sprite.
         self.vel = vec(0, 0)
         keys = pg.key.get_pressed()
         if keys[pg.K_RIGHT]:
@@ -28,6 +39,7 @@ class Player(pg.sprite.Sprite):
             self.vel = vec(0, PLAYER_SPEED)
 
     def update(self):
+        # Method for updated changes/properties of sprite.
         self.get_keys()
         self.rect.center = self.pos
         self.pos += self.vel*self.game.dt
